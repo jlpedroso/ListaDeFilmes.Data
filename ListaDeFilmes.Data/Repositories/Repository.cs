d667a4e53dccab65b30727b7a4811a2041795800
jlpedroso <jlpedroso@yahoo.com.br>
2017-10-26 15:18:49 -0700

    Add files via upload
    Projeto inicial de acesso ao Entity Framework, com repositório e mapa para Diretor (ainda não implementados)

---
+++ ListaDeFilmes.Data/Repositories/Repository.cs
@@ -0,0 +1,53 @@
+﻿using ListaDeFilmes.Domain.Contracts.Repositories;
+using ListaDeFilmes.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeFilmes.Data.Repositories
+{
+    class Repository<T>: IRepository<T> where T: Entity
+    {
+        public T Add(T entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Edit(T entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Del(T entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
