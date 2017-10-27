464d6b29135d8e0be6701f8e5b9867932a6aadf5
jlpedroso <jlpedroso@yahoo.com.br>
2017-10-27 00:09:10 +0000

    Add files via upload
    Criando DataSet

---
+++ ListaDeFilmes.Data/EF/ListaFilmesDataContext.cs
@@ -0,0 +1,23 @@
+﻿using ListaDeFilmes.Domain.Entities;
+using System.Data.Entity;
+
+namespace ListaDeFilmes.Data.EF
+{
+    class ListaFilmesDataContext: DbContext
+    {
+        public ListaFilmesDataContext()
+            : base("FilmesConn")
+        {
+//            Database.SetInitializer(new DbInitializer());
+   //         Configuration.LazyLoadingEnabled = false;  // PARA NÂO ENTRAR EM LOOP NA BUSCA COM INCLUDE // SE ALTERAR O WebApiConfig NÃO PRECISA
+        }
+
+        public DbSet<Diretor> Diretores { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+//            modelBuilder.Configurations.Add(new DiretorMap());
+        }
+
+    }
+}
---
+++ ListaDeFilmes.Data/Repositories/DiretorRepository.cs
@@ -0,0 +1,9 @@
+﻿using ListaDeFilmes.Domain.Contracts.Repositories;
+using ListaDeFilmes.Domain.Entities;
+
+namespace ListaDeFilmes.Data.Repositories
+{
+    class DiretorRepository: Repository<Diretor>, IDiretorRepository
+    {
+    }
+}
